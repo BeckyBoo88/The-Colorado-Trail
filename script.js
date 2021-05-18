c703083677129a35a6cbb1fbd172d7f3a009aa96
BeckyBoo88 <rbooco07@gmail.com>
2021-05-18 11:41:01 -0600

    set variables in JS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let gameStart = document.getElementById('start')
+let welcome= document.getElementById('welcomeScreen')
+let startBtn= document.getElementById('start')
+
+let startScreen= document.getElementById('startScreen')
+let start1Btn= document.getElementById('start1')
+let start2Btn= document.getElementById('start2')
+let start3Btn= document.getElementById('start3')
+
+let solo= document.getElementById('soloSituation')
+let solo1Btn= document.getElementById('solo1')
+let solo2Btn= document.getElementById('solo2')
+let sOption1=document.getElementById('soloOption1')
+let sOption2= document.getElementById('soloOption2')
+
+let friend= document.getElementById('friendSituation')
+let friend1Btn= document.getElementById('friends1')
+let friend2Btn= document.getElementById('friends2')
+let fOption1= document.getElementById('friendsOption1')
+let fOption2= document.getElementById('friendsOption2')
+
+let notReady= document.getElementById('notReadySituation')
+
+let resetBtn= document.getElementsByClassName('reset')
